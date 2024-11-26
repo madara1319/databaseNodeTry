45dafb0127fe549f31a59b5d26f055bce247b363
Mateusz bIK <mateusz.bik@op.pl>
2024-11-26 21:46:57 +0100

    some errors testing with postman

--- controllers/postControllers.js
+++ controllers/postControllers.js
@@ -1,8 +1,15 @@
+const Post = require("../models/Post");
+
 exports.getAllPosts=async (req,res,next)=>{
 res.send("Get all posts route");
 };
 
 exports.createNewPost=async (req,res,next)=>{
+    //const post = New Post("First Post","Body of first post");
+    let {title,body}=req.body;
+    let post = new Post(title,body);
+    post= await post.save();
+    console.log(post);
 res.send("Create new post route");
 };
 
